a52bff16341216f9837095218dd51f58ef652643
DNwin <ndennis@gmail.com>
2015-08-14 02:52:46 -0700

    Initial Commit

---
+++ SocialShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialShare
+//
+//  Created by Dennis Nguyen on 8/14/15.
+//  Copyright (c) 2015 dnwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialShare
+//
+//  Created by Dennis Nguyen on 8/14/15.
+//  Copyright (c) 2015 dnwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
